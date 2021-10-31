6c0c2971cfd02f1fdbf306262fc403b4b6d421c9
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-31 20:38:53 -0300

    Add files via upload

---
+++ 2obradearte.py
@@ -0,0 +1,25 @@
+# Desenhando padrões
+
+from turtle import *
+
+
+speed(11)
+shape("turtle")
+pensize(6)
+color("green")
+
+
+for count in range(100):
+    left(120)
+    forward(80)
+    left(120)
+    forward(80)
+    right(62)
+    forward(80)
+    left(120)
+    forward(80)
+    
+    
+
+   
+
