faf9d42b3761acae86aba94d0e7ee04ad756337c
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-31 20:36:20 -0300

    Add files via upload

---
+++ triangulo.py
@@ -0,0 +1,43 @@
+from turtle import *
+shape("turtle")
+speed(11)
+pensize(7)
+color("Purple")
+
+
+#Equilatero
+for count in range(3):
+    forward(150)
+    left(120)
+penup()
+forward(300)
+pendown()
+
+
+# Isosceles
+color("blue")
+forward(180)
+left(150)
+forward(180)
+left(105)
+forward(87)
+
+penup()
+right(60)
+forward(100)
+pendown()
+
+# Scaleno
+color("yellow")
+forward(100)
+left(60)
+forward(200)
+left(160)
+forward(262)
+right(1450)
+
+
+
+
+
+    
