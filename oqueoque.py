666e4f0a64712f1ccc086e431f645c45885d4ea5
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-11-01 06:52:08 -0300

    Add files via upload

---
+++ oqueoque.py
@@ -0,0 +1,44 @@
+from turtle import *
+speed(11)
+shape("turtle")
+pensize(7)
+color("green")
+
+
+for count in range(27):
+    forward(30)
+    right(90)
+    left(195)
+    forward(30)
+    right(90)
+
+penup()
+right(90)
+forward(200)
+pendown()
+for count in range(30):
+    forward(90)
+    left(90)
+    right(10)
+    forward(10)
+    right(9)
+penup()
+left(90)
+forward(250)
+pendown()
+
+for count in range(27):
+    forward(70)
+    right(90)
+    left(7)
+    forward(1)
+    right(90)
+penup()
+left(90)
+forward(50)
+
+    
+    
+
+
+  
